23f22356d3bb8b77efe845350cf613dd9e7a3dc9
spinklewink <ketchupeggs.beans@gmail.com>
2023-02-13 14:33:32 -0600

    FARTING POF0AIEOMI40 ER9FJMWE9UGM 43RH2EFW
    rocks

---
+++ My project/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+public class ShipMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > 45)
+        {
+            transform.Translate(0, 0, -70);
+        }
+        transform.Translate(0, 0, 0.05f);
+    }
+}
+
+
+
+
+
