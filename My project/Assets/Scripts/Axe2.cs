65ffb8998e5f4797053ae71b4e29532b7fe66773
Santiago11234 <89491348+Santiago11234@users.noreply.github.com>
2023-03-09 20:06:23 -0600

    main menu + axe

---
+++ My project/Assets/Scripts/Axe2.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe2 : MonoBehaviour
+{
+    [Header("Knockback")]
+    public float knockbackForce = 500f;
+    public float knockbackDuration = 0.5f;
+
+    private bool isKnockingBack = false;
+    private Vector3 knockbackDirection;
+
+    [Header("Rotation")]
+    public float maxAngle = 85;
+    public float rotationSpeed = 1;
+
+    void Update()
+    {
+        float angle = Mathf.Sin(Time.time * rotationSpeed) * maxAngle;
+        transform.rotation = Quaternion.Euler(0f ,180f , angle);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isKnockingBack)
+        {
+            Debug.Log("u just got hit L bozo");
+
+            knockbackDirection = other.transform.position - transform.position;
+            knockbackDirection.Normalize();
+
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+    
+            playerRb.AddForce(knockbackDirection * knockbackForce /30, ForceMode.Impulse);
+
+            StartCoroutine(KnockbackCoroutine(playerMovement));
+            
+        }
+    }
+
+    IEnumerator KnockbackCoroutine(PlayerMovement playerMovement)
+    {
+        isKnockingBack = true;
+        yield return new WaitForSeconds(knockbackDuration);
+        isKnockingBack = false;
+    }   
+}
